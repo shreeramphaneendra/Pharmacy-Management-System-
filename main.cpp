28d2a8049f22c1eccc56c21d97f9a24379ca974e
Majeti Shreeram Phaneendra <shreerammajeti@gmail.com>
2025-04-29 23:17:35 +0530

    made final changes to the code

--- main.cpp
+++ main.cpp
@@ -4,16 +4,15 @@
 #include <vector>
 using namespace std;
 
-class medicine 
-{
-    public:
-        string name;
-        int expDate;
-        int quantity;
-        float price;
-        int id;
-        string category;
-    };
+class medicine {
+public:
+    string name;
+    int expDate;
+    int quantity;
+    float price;
+    int id;
+    string category;
+};
 vector<medicine> uploadfile()
 {
     ifstream infile("/Users/shreeramssp/Desktop/inventorypharamacy.txt");
@@ -24,7 +23,7 @@ vector<medicine> uploadfile()
         cout<<" ERROR file not opening ";
         return meds;
     }
-    while(infile>>m.id>>m.name>>m.quantity>>m.expDate>>m.price)
+    while(infile>>m.id>>m.name>>m.quantity>>m.expDate>>m.price>>m.category)
     {
         meds.push_back(m);
     }
@@ -89,6 +88,7 @@ void saveInventory(const vector<medicine>& meds)
 void billing(vector<medicine> &meds)
 {
     float total =0.0;
+    vector<medicine> medList;
     char choice;
     do
     {
@@ -98,35 +98,41 @@ void billing(vector<medicine> &meds)
         medicine *m=searchName(meds,name);
         if (m)
         {
-            int qty;
-            cout << "Enter quantity: ";
-            cin >> qty;
+                    int qty;
+                    cout << "Enter quantity: ";
+                    cin >> qty;
 
-            if (m->quantity >= qty)
-            {
-                float cost = qty * m->price;
-                total += cost;
+                    if (m->quantity >= qty)
+                    {
+                        float cost = qty * m->price;
+                        total += cost;
 
-                updateStock(*m, qty);
-                cout << "Added to bill: " << m->name << " x" << qty << " = ₹" << cost << "\n";
-             }
-            else
-            {
-                cout << "Only " << m->quantity << " units available.\n";
-            }
-        }
-        else
-        {
-            cout << "Medicine not found.\n";
-        }
+                        updateStock(*m, qty);
+                        cout << "Added to bill: " << m->name << " x" << qty << " = ₹" << cost << "\n";
+                        medList.push_back(*m);
+                    }
+                    else
+                    {
+                        cout << "Only " << m->quantity << " units available.\n";
+                    }
+                }
+             else
+                {
+                    cout << "Medicine not found.\n";
+                }
 
-        cout << "Do you want to buy another medicine? (y/n): ";
-         cin >> choice;
+                cout << "Do you want to buy another medicine? (y/n): ";
+                cin >> choice;
 
-    } while (choice == 'y' || choice == 'Y');
-    
-    cout << "\n======= Final Bill =======\n";
+            } while (choice == 'y' || choice == 'Y');
+
+            cout << "\n======= Final Bill =======\n";
         cout << "Total amount: ₹" << total << "\n";
+         for(auto &m:medList)
+        {
+            cout << "ID: " << m.id << "\nName: " << m.name << "\nQuantity: " << m.quantity
+                 << "\nExpiry: " << m.expDate << "\nPrice: ₹" << m.price << "\nCategory: " << m.category << "\n";
+        }
         cout << "==========================\n";
 
     saveInventory(meds);
@@ -145,17 +151,17 @@ int main()
         cout << "\n=================================\n";
         cout << "     Pharmacy Inventory System\n";
         cout << "=================================\n";
-        cout << "1. Search Medicine by ID\n";
-        cout << "2. Search Medicine by Name\n";
-        cout << "3. Search Medicine by Category\n";
+        cout << "1. Search Medicine by category\n";
+        cout << "2. Search Medicine by ID\n";
+        cout << "3. Search Medicine by name\n";
         cout << "4. Purchase Medicines (Billing)\n";
         cout << "5. Exit\n";
         cout << "---------------------------------\n";
         cout << "Enter your choice: ";
         cin >> choice;
-        switch (choice) 
+        switch (choice)
         {
-            case 1: {
+            case 2: {
                 int id;
                 cout << "Enter ID to search: ";
                 cin >> id;
@@ -170,7 +176,7 @@ int main()
                 break;
             }
 
-            case 2: {
+            case 3: {
                 string name;
                 cout << "Enter Name to search: ";
                 cin >> name;
@@ -185,8 +191,9 @@ int main()
                 break;
             }
 
-            case 3: {
+            case 1: {
                 string category;
+                cout<<"analgesic"<<endl<<"antacid"<<endl<<"antibiotic"<<endl<<"antidiabetic"<<endl<<"antidepressant"<<endl<<"antihistamine"<<endl<<"antihypertensive"<<endl<<"antifungal"<<endl<<"antiviral"<<endl<<"bronchodilator"<<endl<<"diuretic"<<endl<<"general"<<endl<<"pregnancy"<<endl<<"statin"<<endl<<"steroid"<<endl<<"vitamin"<<endl;
                 cout << "Enter Category to search: ";
                 cin >> category;
                 searchCategory(meds, category);
